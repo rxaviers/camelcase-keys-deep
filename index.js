fbe5bd16ec7c47bd4f825e882aaa6de82c145444
Rafael Xavier de Souza <rxaviers@gmail.com>
2016-04-15 22:57:35 -0300

    Use own recursion

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var camelcase = require("camelcase");
 var mapObj = require("map-obj");
 
-module.exports = function(obj) {
-  return JSON.parse(JSON.stringify(obj, function(key, value) {
-    if (typeof value === "object" && !Array.isArray(value)) {
-      value = mapObj(value, function(_key, _value) {
-        var newKey = camelcase(_key);
-        if (_key !== newKey && newKey in value) {
-          throw new Error("Camelcased key `" + newKey + "` would overwrite existing key of the given JSON object");
-        }
-        return [newKey, _value];
-      });
-    }
-    return value;
-  }));
+module.exports = function camelcaseKeysDeep(obj) {
+  // Any falsy, which includes `null` whose typeof is `object`.
+  if (!obj) {
+    return obj;
+  }
+  // Date, whose typeof is `object` too.
+  if (obj instanceof Date) {
+    return obj;
+  }
+  // Array, whose typeof is `object` too.
+  if (Array.isArray(obj)) {
+    return obj.map(function(element) {
+      return camelcaseKeysDeep(element);
+    });
+  }
+  // So, if this is still an `object`, we might be interested in it.
+  if (typeof obj === "object") {
+    return mapObj(obj, function(key, value) {
+      var newKey = camelcase(key);
+      if (key !== newKey && newKey in obj) {
+        throw new Error("Camelcased key `" + newKey + "` would overwrite existing key of the given JSON object");
+      }
+      return [newKey, camelcaseKeysDeep(value)];
+    });
+  }
+  // Something else like a String or Number.
+  return obj;
 }
--- test/index.js
+++ test/index.js
@@ -5,8 +5,21 @@ describe("camelcaseKeysDeep", function() {
   var extract;
 
   it("should deeply camelcase the keys of a JSON object", function() {
-    var json = {unicorn_rainbow: {foo_bar: 1}};
-    expect(camelcaseKeysDeep(json)).to.be.deep.equal({unicornRainbow: {fooBar: 1}});
+    var aDate = new Date(2016, 3, 15);
+    var json = {
+      unicorn_rainbow: {
+        foo_bar: 1,
+        a_date: aDate,
+        an_array: [1, 2, {foo_bar: 3}]
+      }
+    };
+    expect(camelcaseKeysDeep(json)).to.be.deep.equal({
+      unicornRainbow: {
+        fooBar: 1,
+        aDate: aDate,
+        anArray: [1, 2, {fooBar: 3}]
+      }
+    });
   });
 
   it("should raise if camelcased key would overwrite existing key of the JSON object", function() {
